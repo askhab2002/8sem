d7a19be153d8d968957f31e832ffc6b7281ddc44
askhab2002 <askhab2002@icloud.com>
2024-05-27 21:22:51 +0500

    PDE_1

--- PDE_1/ImplicitMethod.cpp
+++ PDE_1/ImplicitMethod.cpp
@@ -7,6 +7,12 @@ using namespace std;
 
 
 void Sweep(double *y, double *a, double *b, double *c, double *f, int N) {
+/*
+        for(int i = 0; i < N - 1; i++) {
+		a[i] = -a[i];
+		b[i] = -b[i];
+	}
+*/
 	double *alpha = (double *)calloc(N, sizeof(double));
 	double *beta = (double *)calloc(N, sizeof(double));
 
@@ -75,14 +81,14 @@ double **SweepMethod(function_pointer_2 f, function_pointer_1 p, function_pointe
 
 
 	for(int i = 1; i < N - 2; i++) {
-		a[i] = -1/(h * h);
-		b[i] = -1/(h * h);
+		a[i] = 1/(h * h);
+		b[i] = 1/(h * h);
 	}
 
-	a[0] = -2/(h * h);
-	a[N - 2] = -1/(h * h);
-	b[0] = -1/(h * h);
-	b[N - 2] = -1;
+	a[0] = 2/(h * h);
+	a[N - 2] = 1/(h * h);
+	b[0] = 1/(h * h);
+	b[N - 2] = 1;
 
 	for(int i = 0; i < N; i++) {
 		u[0][i] = (*u_0)(i * h);
@@ -93,15 +99,21 @@ double **SweepMethod(function_pointer_2 f, function_pointer_1 p, function_pointe
 	for(int i = 1; i < T; i++) {
 		for(int j = 0; j < N - 1; j++) {
 			c[j] = (1/tau) + (2/(h * h)) + (*p)(j * h);
+//			cout << c[j] << " ";
 		}
 		c[N - 1] = 1;
+//		cout << c[N - 1] << endl;
 
 		for(int j = 0; j < N - 1; j++) {
-			F[j] = (u[i - 1][j]/tau) + (*f)((i - 1) * tau, j * h);
+			F[j] = (u[i - 1][j]/tau) + (*f)(i * tau, j * h);
+//			cout << F[j] << " ";
 		}
+//		cout << endl;
                 
 		//cout << " Tau = " << tau << " h = " << h << endl;
-		Sweep(u[i], a, b, c, F, N - 1);
+		Sweep(u[i], b, a, c, F, N - 1);
+		u[i][N - 1] = u[i][N - 2];
+
 		//cout << "a = " << a[0] << " b = " << b[0] << " c = " << c[0] << endl;;
 	}
 
--- PDE_1/tests.cpp
+++ PDE_1/tests.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <stdio.h>
 #include <math.h>
+#include <fstream>
 #include "ImplicitMethod.hpp"
 #define eps 1e-8
 
@@ -50,34 +51,109 @@ double function6(double t, double x) {
         return cos(M_PI * x) * exp(-M_PI * M_PI * t);
 }
 
+
 double function7(double x, double t) {
         double k = x;
 	k = t;
 	k = 0;
 
 	return k;
-}	
+}
+
+double function8(double x){
+
+	return cos(M_PI * x) + cos(M_PI * 2 * x) + cos(M_PI * 3 * x) + cos(M_PI * 4 * x);
+}
+
+double function9(double t, double x) {
+
+	double sum1 = cos(M_PI * x) * exp(-M_PI * M_PI * t) + cos(M_PI * 2 * x) * exp(-M_PI * M_PI * 4 * t);
+	double sum2 = cos(M_PI * 3 * x) * exp(-M_PI * M_PI * 9 * t) + cos(M_PI * 4 * x) * exp(-M_PI * M_PI * 16 * t);
+
+	return sum1 + sum2;
+}
+
+void result_out(double **u, double **u_real, int N, int T, string filename) {
+
+	fstream out;
+        out.open(filename, std::ofstream::out | std::ofstream::trunc);
+
+	double *x_grid = (double *)calloc(N, sizeof(double));
+	double *y_grid = (double *)calloc(T, sizeof(double));
+
+	double h = 1 / ((double)N - 0.5);
+	double t = 1 / ((double)T);
+
+	for(int i = 0; i < N; i++) {
+		x_grid[i] = i * h;
+	}
+
+	for(int i = 0; i < T; i++) {
+		y_grid[i] = i * t;
+	}
+
+	for(int i = 0; i < N; i++) {
+
+		for(int j = 0; j < T; j++) {
+			out << x_grid[i] << " " << y_grid[j] << " " << u[i][j] << " " << u_real[i][j] << endl;
+
+		}
+	}
+
+	free(x_grid);
+	free(y_grid);
+
+	out.close();
+}
+
+function_pointer_1 choose_func(int k) {
+
+	switch(k) {
+		case 1:
+			return function1;
+		case 2:
+                        return function2;
+		case 3:
+                        return function3;
+		case 4:
+                        return function4;
+		case 5:
+                        return function5;
+                case 8:
+                        return function8;
+	}
+
+	return function1;
+
+}
+
+function_pointer_2 choose_func_2(int k) {
+
+        switch(k) {
+                case 6:
+                        return function6;
+		case 7:
+			return function7;
+                case 9:
+                        return function9;
+        }
+
+        return function6;
+
+}
 
 
 
-int main(void) {
+void test(int func1, int func2, int func3, int func4, int T, int N) {
 
-	function_pointer_1 u_0 = function5;
-	function_pointer_1 p = function4;
-	function_pointer_2 f = function7;
+	function_pointer_1 u_0 = choose_func(func1); //function5;
+	function_pointer_1 p = choose_func(func2); //function4;
+	function_pointer_2 f = choose_func_2(func3); //function7;
 
-	int T = 3;
-        int N = 10;
-
-	cout << " Введите число разбиения пространственной координаты" << endl;
-	cin >> N;
-
-	cout << " Введите число разбиения временной координаты" << endl;
-	cin >> T;
 
 	double **u = SweepMethod(f, p, u_0, T, N);
 
-	function_pointer_2 u_real_ = function6;
+	function_pointer_2 u_real_ = choose_func_2(func4); //function6
 
 	double h = 1/((double)N - 0.5);
 	double t = 1/(double)T;
@@ -121,7 +197,7 @@ int main(void) {
 	for(int i = 0; i < T; i++) {
 		for(int j = 0; j < N - 1; j++) {
 
-			error_mean += fabs(u[i][j] - u_real[i][j]) * fabs(u[i][j] - u_real[i][j]);
+			error_mean += fabs(u[i][j] - u_real[i][j]);
 
 			if(L_0_error < fabs(u[i][j] - u_real[i][j])) {
 				L_0_error = fabs(u[i][j] - u_real[i][j]);
@@ -136,9 +212,12 @@ int main(void) {
 		}
 	}
 
-	cout << " L_0 error = " << L_0_error << " L_0 relative error = " << L_0_relative_error << endl;
-        cout << " error mean disp = " << sqrt(error_mean) << endl;
+	double mean_error = error_mean/(N * T);
 
+	cout << " L_0 error = " << L_0_error << " L_0 relative error = " << L_0_relative_error << endl;
+        cout << " error mean disp = " << mean_error << endl;
+
+	result_out(u, u_real, N, T, "result.txt");
 
 	for(int i = 0; i < T; i++) {
 		free(u[i]);
@@ -146,6 +225,34 @@ int main(void) {
 	}
 	free(u);
 	free(u_real);
+	
+
+	return;
+}
+
+int main(void) {
+	int T = 10;
+	int N = 10;
+
+	cout << " Введите число разбиения пространственной координаты" << endl;
+        cin >> N;
+
+        cout << " Введите число разбиения временной координаты" << endl;
+        cin >> T;
+
+	int func_u_0 = 8; //5;
+	int func_p = 4;
+	int func_f = 7; //7;
+	int func_solution = 9; //6;
+
+	//test(func_u_0, func_p, func_f, func_solution, T, N);
+	
+	func_u_0 = 5;
+        func_p = 4;
+        func_f = 7;
+        func_solution = 6;
+
+        test(func_u_0, func_p, func_f, func_solution, T, N);
 	
 
 	return 0;
